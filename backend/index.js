41283fd3346e8e8ec9b18b2b7a61a5d2c178d134
Igor <rmnv.qqq@gmail.com>
2022-12-03 23:22:37 +0300

    feature: added multer & file upload

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,7 @@
 import express from 'express'
 import mongoose from 'mongoose'
 import dotenv from 'dotenv'
+import multer from 'multer'
 import * as UserController from './controllers/UserController.js'
 import * as PostController from './controllers/PostController.js'
 import { registerValidation, loginValidation, postCreateValidation } from './utils/validations.js'
@@ -15,12 +16,32 @@ mongoose.connect(process.env.MONGO_DATABASE_LINK)
 
 const app = express()
 
+const storage = multer.diskStorage({
+  destination: (_, __, cb) => {
+    cb(null, 'uploads');
+  },
+  filename: (_, file, cb) => {
+    cb(null, file.originalname)
+  }
+})
+
+const upload = multer({ storage })
+
 app.use(express.json())
+// for work /w static files
+app.use('/uploads', express.static('uploads'))
 
 // authorization
 app.post('/auth/login', loginValidation, UserController.login)
 app.post('/auth/register', registerValidation, UserController.register)
 app.get('/auth/me', checkAuth, UserController.getMe)
+
+// file upload
+app.post('/uploads', checkAuth, upload.single('image'), (req, res) => {
+  res.json({
+    url: `/uploads/${req.file.originalname}`
+  })
+})
 
 // posts
 app.get('/posts', PostController.getAll)
